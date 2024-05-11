303fab742ea475c164c418d2af2857f7e543c4cd
Arman09890 <145832862+Arman09890@users.noreply.github.com>
2024-05-11 10:35:03 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+// // let arr = [1,2,3,4,5];
+// // if(arr[0]>arr[1]){
+// //     alert('444');
+// // }else alert('555');
+
+// // for(let i = 1; i < 10 ; i++){
+// // console.log(i);
+// // }
+
+// // for(let  i = 9; i >= 1; i--){
+// //     console.log(i);
+// // }
+
+// for(let i = 0; i<= 10; i+=2){
+//     console.log(i);
+// }
+
+// for(let i = 1; i<= 10; i+=2){
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i++){
+//     if(i%2 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// for (let i = 0; i < 10; i++){
+//     if(i%2 = 1){
+//         console.log(i);
+//     }
+// }
+
+// let arr = [1,2,3,4,5,6,7]; 
+// for(let i = 1; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// for(let elem of){
+//     console.log(elem);
+// }
+
+// for(let  i = 0; i < arr.length; i++){
+//     if(arr[i] == 3){
+//         console.log(arr[i]);
+//         break;
+//     }
+// }
+
+// for(let  i = 0; i < arr.length; i++){
+//     if(arr[i] == 3){
+//         console.log(arr[i]);
+//         continue;
+//     }
+// }
+
+
+
+
+for(let i = 0; i <= 100; i++);{
+    console.log(i);
+}
+
+
+for(let i = 11; i <= 33; i++);{
+    console.log(i);
+}
+
+
+for(let i = 0; i <= 100; i++);{
+    if(i%2 == 0)
+    console.log(i);
+}
+
+
+for(let i = 1; i<= 99; i++);{
+    if(i%2)
+    console.log(i);
+}
+
+
+for(let i = 100; i>= 0; i--);{
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
